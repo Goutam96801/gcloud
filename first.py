2659c71faf0f6629d713568b1b3860fb518c1e2b
Goutam Kumar Choudhary <gautamkumar96801@gmail.com>
2024-03-04 23:27:51 +0530

    Update first.py

--- first.py
+++ first.py
@@ -1,1 +1,15 @@
+def check_odd_even(num):
+    if num % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+
+# Input
+number = int(input("Enter a number: "))
+
+# Check if the number is odd or even
+result = check_odd_even(number)
+
+# Output
+print(f"The number {number} is {result}.")
 
